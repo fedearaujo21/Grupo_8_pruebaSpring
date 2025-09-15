ca4a682ea885d3831f48e0c260a9fc0be489278f
Manolo <manugalan0327@gmail.com>
2025-09-15 17:22:17 -0300

    cambios

--- pruebaSpring/src/main/java/ar/edu/unlpam/ing/pruebaSpring/tp1/controller/ConversorController.java
+++ pruebaSpring/src/main/java/ar/edu/unlpam/ing/pruebaSpring/tp1/controller/ConversorController.java
@@ -1,4 +1,19 @@
 package ar.edu.unlpam.ing.pruebaSpring.tp1.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
 public class ConversorController {
+    @GetMapping("/celsiusAfahrenheit/{celsius}")
+    public Map<String, Object> convertir(@PathVariable double celsius){
+        double fahrenheit = celsius * 9 / 5 + 32;
+        return Map.of(
+                "celsius", celsius,
+                "fahrenheit", fahrenheit
+        );
+    }
 }
