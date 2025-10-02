995e31db03c65606dd22e9c64e98e809a51b37fd
Simón Martin <101838886+SimonMartin1@users.noreply.github.com>
2025-10-02 19:54:19 -0300

    remove IOE exception

--- tecnoRAEE/src/main/java/Grupo8/tecnoRAEE/dto/ResiduoImp.java
+++ tecnoRAEE/src/main/java/Grupo8/tecnoRAEE/dto/ResiduoImp.java
@@ -1,9 +1,9 @@
-package Grupo8.tecnoRAEE.dto;
+package grupo8.tecnoRAEE.dto;
 
 import java.util.List;
 
-import Grupo8.tecnoRAEE.dao.*;
-import Grupo8.tecnoRAEE.model.*;
+import grupo8.tecnoRAEE.dao.*;
+import grupo8.tecnoRAEE.model.*;
 import org.springframework.stereotype.Service;
 import org.sql2o.Connection;
 
@@ -18,7 +18,7 @@ public class ResiduoImp implements ResiduoDao {
     }
 
     @Override
-    public List<Residuo> listarValidos() throws RuntimeException, ResiduoDAOException {
+    public List<Residuo> listarValidos(){
         List<Residuo> res;
         String sql = "Select * from residuos where es_valido=1;";
         try (Connection con = Sql2oDAO.getSql2o().open()) {
@@ -26,8 +26,6 @@ public class ResiduoImp implements ResiduoDao {
                     .createQuery(sql)
                     .executeAndFetch(Residuo.class);
             return res;
-        } catch (Exception e) {
-            throw new InternalError("Error de conexion", e);
         }
     }
 }
