65902718c6bf468ed9962698d1214eb6d37b82e1
Simón Martin <101838886+SimonMartin1@users.noreply.github.com>
2025-10-02 02:55:33 -0300

    modify Dao

--- tecnoRAEE/src/main/java/Grupo8/tecnoRAEE/model/ResiduoImp.java
+++ tecnoRAEE/src/main/java/Grupo8/tecnoRAEE/model/ResiduoImp.java
@@ -1,6 +1,9 @@
 package grupo8.tecnoRAEE.model;
 
+
 import java.util.List;
+
+import org.sql2o.Connection;
 
 public class ResiduoImp implements ResiduoDao {
     private final Sql2oDAO coneccion;
@@ -9,7 +12,23 @@ public class ResiduoImp implements ResiduoDao {
         coneccion= new Sql2oDAO();
     }
 
+    @Override
     public List<Residuo> getResiduos(){
+        
+        
         return null;
     }
+
+    @Override
+    public List<Residuo> listarValidos(){
+        List<Residuo> res;
+        String sql = "Select * from producto;";
+        try (Connection con = Sql2oDAO.getSql2o().open()) {
+        res = con
+                .createQuery(sql)
+                .executeAndFetch(Residuo.class);
+        }
+        return res;
+    }        
+
 }
