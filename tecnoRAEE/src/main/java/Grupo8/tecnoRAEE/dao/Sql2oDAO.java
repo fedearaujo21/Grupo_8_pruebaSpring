19b9571c35a336ab61897fc42198ce0579db4ef4
Simón Martin <101838886+SimonMartin1@users.noreply.github.com>
2025-10-02 22:49:46 -0300

    Add ResiduoImp bean

--- tecnoRAEE/src/main/java/Grupo8/tecnoRAEE/dao/Sql2oDAO.java
+++ tecnoRAEE/src/main/java/Grupo8/tecnoRAEE/dao/Sql2oDAO.java
@@ -1,17 +1,16 @@
 package grupo8.tecnoRAEE.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.sql2o.Sql2o;
 
-
+@Configuration
 public class Sql2oDAO {
-    private static Sql2o sql2o;
+    private Sql2o sql2o;
 
-    public static Sql2o getSql2o() {
-        if (sql2o == null) {
-            sql2o = new Sql2o("jdbc:mysql://localhost:3306/tecno_raee_db", "root", "");
-
-        }
-        return sql2o;
+    @Bean
+    public Sql2o Sql2o() {
+            return new Sql2o("jdbc:mysql://localhost:3306/tecno_raee_db", "root", "");
     }
 
 }
--- tecnoRAEE/src/main/java/Grupo8/tecnoRAEE/dto/ResiduoImp.java
+++ tecnoRAEE/src/main/java/Grupo8/tecnoRAEE/dto/ResiduoImp.java
@@ -4,12 +4,24 @@ import java.util.List;
 
 import grupo8.tecnoRAEE.dao.*;
 import grupo8.tecnoRAEE.model.*;
-import org.springframework.stereotype.Service;
-import org.sql2o.Connection;
 
-@Service
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+@Configuration
+@AllArgsConstructor
 public class ResiduoImp implements ResiduoDao {
 
+    private final Sql2o sql2o;
+
+    @Bean
+    public ResiduoImp Residuo(Sql2o sql2o){
+        return new ResiduoImp(sql2o);
+    }
 
     @Override
     public List<Residuo> getResiduos() {
@@ -21,7 +33,7 @@ public class ResiduoImp implements ResiduoDao {
     public List<Residuo> listarValidos(){
         List<Residuo> res;
         String sql = "Select * from residuos where es_valido=1;";
-        try (Connection con = Sql2oDAO.getSql2o().open()) {
+        try (Connection con = sql2o.open()) {
             res = con
                     .createQuery(sql)
                     .executeAndFetch(Residuo.class);
