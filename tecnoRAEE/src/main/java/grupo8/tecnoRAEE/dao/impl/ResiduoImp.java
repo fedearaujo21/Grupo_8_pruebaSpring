dfdaa5998891579203e1e5dffdb26c6c5d163ec0
Simón Martin <101838886+SimonMartin1@users.noreply.github.com>
2025-10-31 17:28:35 -0300

    fix findById return type

--- tecnoRAEE/src/main/java/grupo8/tecnoRAEE/dao/impl/ResiduoImp.java
+++ tecnoRAEE/src/main/java/grupo8/tecnoRAEE/dao/impl/ResiduoImp.java
@@ -45,17 +45,15 @@ public class ResiduoImp implements ResiduoDao {
     }
 
     @Override
-    public Optional<Residuo> findById(Long id) {
+    public Residuo findById(Long id) {
         String sql = "SELECT * FROM residuos WHERE id = :id";
         try (Connection con = sql2o.open()) {
             // executeAndFetchFirst devuelve el objeto o null si no lo encuentra.
-            Residuo residuo = con.createQuery(sql)
+
+            return con.createQuery(sql)
                     .addParameter("id", id)
                     .executeAndFetchFirst(Residuo.class);
 
-            // Optional.ofNullable() crea un Optional vacío si el residuo es null,
-            // o un Optional con el residuo si fue encontrado.
-            return Optional.ofNullable(residuo);
         }
     }
 
