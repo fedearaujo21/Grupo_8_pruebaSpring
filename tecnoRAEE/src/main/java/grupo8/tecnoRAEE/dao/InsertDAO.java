1b5c35a3adf4decf9bd2615c987abc487273a5cb
Simón Martin <101838886+SimonMartin1@users.noreply.github.com>
2025-11-04 12:35:27 -0300

    Add reflexive insert abstract class

---
+++ tecnoRAEE/src/main/java/grupo8/tecnoRAEE/dao/InsertDAO.java
@@ -0,0 +1,75 @@
+package grupo8.tecnoRAEE.dao;
+
+import grupo8.tecnoRAEE.model.Tabla;
+import lombok.AllArgsConstructor;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+import java.lang.reflect.Field;
+
+
+public class InsertDAO {
+
+    public InsertDAO(Sql2o sql2o){
+        this.sql2o = sql2o;
+    }
+
+    private final Sql2o sql2o;
+
+
+    public <T> Long insertar_key(T entidad) throws Exception {
+        return ejecutarInsert(entidad);
+    }
+
+
+    public <T> void insertar(T entidad) throws Exception {
+        ejecutarInsert(entidad);
+    }
+
+    private <T> Long ejecutarInsert(T model) throws Exception {
+        Class<?> clase = model.getClass();
+
+        Field[] campos = clase.getFields();
+
+        StringBuilder columnas = new StringBuilder();
+        StringBuilder valores = new StringBuilder();
+
+        //Obtenemos la anotacion
+        String nombreTabla = clase.getAnnotation(Tabla.class).nombre();
+
+
+        for (Field f : campos) {
+            f.setAccessible(true);
+            Object valor = f.get(model);
+            if (valor != null) {
+                if (!columnas.isEmpty()) {
+                    columnas.append(", ");
+                    valores.append(", ");
+                }
+                columnas.append(f.getName());
+                valores.append(":").append(f.getName());
+            }
+        }
+
+
+        String sql = "INSERT INTO " + nombreTabla + " (" + columnas + ") VALUES (" + valores + ")";
+
+        try (Connection con = sql2o.open()) {
+            var query = con.createQuery(sql, true); // true → devuelve la clave generada
+
+            // Agregamos los parámetros dinámicamente
+            for (Field f : campos) {
+                f.setAccessible(true);
+                Object valor = f.get(model);
+                if (valor != null) {
+                    query.addParameter(f.getName(), valor);
+                }
+            }
+            // Ejecutamos y devolvemos la clave generada
+            return query.executeUpdate().getKey(Long.class);
+        } catch (Exception e) {
+            throw new Exception("Error guardando " + nombreTabla, e);
+        }
+    }
+}
+
